0945b524ad9bf37451504fa4d3d5ef2519c914f9
unknown <you@example.com>
2024-02-25 13:30:06 +0200

    all

--- src/App.js
+++ src/App.js
@@ -1,24 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
+// App.js
+
+import React, { useState, useEffect } from "react";
+import styled from "styled-components";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import NameForm from "./components/NameForm";
+import RoomsList from "./components/RoomsList";
+import Game from "./components/Game";
+import { initRoomsFunc } from "./helpers/init"; // Import the initialization function
+// FOR RUNNINIG
+const AppContainer = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  margin-top: 20px;
+  background-color: snow;
+`;
+
+// const Title = styled.h1`
+//   margin-top: 15px;
+//   font-size: 4rem;
+// `;
+////console.log("12-12-12-12")
 
 function App() {
+  const [userName, setUserName] = useState("");
+  const [roomsInitialData, setRoomsInitialData] = useState(null); // State to hold initialization data
+  const [dataIsSet, setDataIsSet] = useState(false); // State to hold initialization data
+
+  useEffect(() => {
+    // Initialize the app when it mounts
+    const init = async () => {
+      ////console.log("6666")
+
+      try {
+        const initRoomsObj = await initRoomsFunc();
+        setRoomsInitialData(initRoomsObj);
+      } catch (error) {
+        ////console.error("Error initializing the app:", error);
+      }
+    };
+    init();
+  }, []);
+
+  
+  useEffect(() => {
+    ////console.log("5555")
+
+    if  ( roomsInitialData )  {
+      setDataIsSet(true)
+      ////console.log("IN App roomsInitialData -- ", roomsInitialData)
+    }
+  }, [roomsInitialData]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <AppContainer>
+        <Routes>
+{ dataIsSet? <Route path="/rooms" element={<RoomsList userName={userName} roomsInitialData={roomsInitialData} />} /> : null}
+          <Route path="/name" element={<NameForm setUserName={setUserName} />} />
+          {/* Add the route for the game page with :roomId parameter */}
+          <Route path="/game/:roomId" element={<Game />} />
+
+          <Route path="" element={<NameForm setUserName={setUserName} />} />
+        </Routes>
+      </AppContainer>
+    </Router>
   );
 }
 
